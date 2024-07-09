0be54e44f5f905316c65614c34bb1379c50f25b3
Trong_Hieu <HieuPTHE173089@fpt.edu.vn>
2024-07-09 09:06:20 +0700

    update
    ok

---
+++ Product Management System/Data/ProductPriceHistoryDAO.cs
@@ -0,0 +1,45 @@
+﻿using Product_Management_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_Management_System.Data
+{
+    public class ProductPriceHistoryDAO
+    {
+        public static List<ProductPriceHistory> GetAllPriceHistories()
+        {
+            ProductManagementDbContext dbContext = new ProductManagementDbContext();
+            return dbContext.ProductPriceHistories.ToList();
+        }
+
+        public static void InsertPriceHistory(ProductPriceHistory priceHistory)
+        {
+            ProductManagementDbContext dbContext = new ProductManagementDbContext();
+            dbContext.ProductPriceHistories.Add(priceHistory);
+            dbContext.SaveChanges();
+        }
+
+        public static void UpdatePriceHistory(ProductPriceHistory priceHistory)
+        {
+            ProductManagementDbContext dbContext = new ProductManagementDbContext();
+            dbContext.ProductPriceHistories.Update(priceHistory);
+            dbContext.SaveChanges();
+        }
+
+        public static void DeletePriceHistory(ProductPriceHistory priceHistory)
+        {
+            ProductManagementDbContext dbContext = new ProductManagementDbContext();
+            dbContext.ProductPriceHistories.Remove(priceHistory);
+            dbContext.SaveChanges();
+        }
+
+        public static ProductPriceHistory? GetPriceHistoryById(int id)
+        {
+            ProductManagementDbContext dbContext = new ProductManagementDbContext();
+            return dbContext.ProductPriceHistories.Find(id);
+        }
+    }
+}
