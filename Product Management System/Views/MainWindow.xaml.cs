3e988480d6115868c521b494d15033bd1e7d8e2a
Trong_Hieu <HieuPTHE173089@fpt.edu.vn>
2024-07-09 10:02:43 +0700

    updATE CS

--- Product Management System/Views/MainWindow.xaml.cs
+++ Product Management System/Views/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Product_Management_System.Models;
+using Product_Management_System.Views.ProductCostHistory;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +18,16 @@ namespace Product_Management_System
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ProductManagementDbContext dbcontext;
         public MainWindow()
         {
             InitializeComponent();
+            dbcontext = new ProductManagementDbContext();
+        }
+
+        private void btnCostHistory_Click(object sender, RoutedEventArgs e)
+        {
+            frProductCostHistory.Content = new ProductCostHistoryPage();
         }
     }
 }
