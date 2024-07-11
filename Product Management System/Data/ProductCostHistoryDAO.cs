c55960428d1e212a2d69ed964eb57e1360a28e1f
Trong_Hieu <HieuPTHE173089@fpt.edu.vn>
2024-07-11 18:08:17 +0700

    ok

---
+++ Product Management System/Data/ProductCostHistoryDAO.cs
@@ -0,0 +1,45 @@
+﻿using Product_Management_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_Management_System.Data
+{
+    public class ProductCostHistoryDAO
+    {
+        public static List<ProductCostHistory> GetAllCostHistories()
+        {
+            ProductManagementDbContext dbContext = new ProductManagementDbContext();
+            return dbContext.ProductCostHistories.ToList();
+        }
+
+        public static void InsertCostHistory(ProductCostHistory costHistory)
+        {
+            ProductManagementDbContext dbContext = new ProductManagementDbContext();
+            dbContext.ProductCostHistories.Add(costHistory);
+            dbContext.SaveChanges();
+        }
+
+        public static void UpdateCostHistory(ProductCostHistory costHistory)
+        {
+            ProductManagementDbContext dbContext = new ProductManagementDbContext();
+            dbContext.ProductCostHistories.Update(costHistory);
+            dbContext.SaveChanges();
+        }
+
+        public static void DeleteCostHistory(ProductCostHistory costHistory)
+        {
+            ProductManagementDbContext dbContext = new ProductManagementDbContext();
+            dbContext.ProductCostHistories.Remove(costHistory);
+            dbContext.SaveChanges();
+        }
+
+        public static ProductCostHistory? GetCostHistoryById(int id)
+        {
+            ProductManagementDbContext dbContext = new ProductManagementDbContext();
+            return dbContext.ProductCostHistories.Find(id);
+        }
+    }
+}
---
+++ Product Management System/Repositories/CostHistory/ICostHistoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_Management_System.Repositories.CostHistory
+{
+    interface ICostHistoryRepository
+    {
+    }
+}
